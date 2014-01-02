1ed70ef7e8772be2a9113f64f5f0e8807a28d384
jack.ocleebrown <jack.ocleebrown@googlemail.com>
2014-01-02 23:21:08 +0000

    Correct version number

--- src/cmdline.h
+++ src/cmdline.h
@@ -32,7 +32,7 @@ extern "C" {
 
 #ifndef CMDLINE_PARSER_VERSION
 /** @brief the program version */
-#define CMDLINE_PARSER_VERSION "0.1Rev30"
+#define CMDLINE_PARSER_VERSION "0.1Rev51"
 #endif
 
 /** @brief Where the command line options are stored */
